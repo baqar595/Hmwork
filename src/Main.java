37dde1717be8933607665cf83434e1d21b8788ca
Sussybaqara <baqarbuxsianidze@gmail.com>
2024-11-18 21:28:02 +0400

    asdasda
    ASDASD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        scanner.nextInt(i);
+        System.out.println(i);
+        System.out.println("negro");
+
+
+
+
+
     }
 }
